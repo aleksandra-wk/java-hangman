f0a92402dc3a20df8bb4aa612b279bbc81807a77
ola wandzilak <aleksandra.wandzilak@gmail.com>
2020-03-01 10:05:23 +0100

    I added a sceleton of class play

---
+++ src/main/java/pl/edu/agh/hangman/play.java
@@ -0,0 +1,34 @@
+package pl.edu.agh.hangman;
+
+public class play {
+	
+	private String wordToGuess;
+	private int nrOfTrials = 7;
+	private char[] emptyGuess = null;
+//	char currentChar = null;
+	
+	
+	//split the word into chars
+	private char[] splitToChars(String word) {
+		char[] letters=null;
+		return letters;
+	}
+	
+	//create empty string of proper length
+	private char[] emptyGuess(int length) {
+		char[] guess = null;
+		return guess;
+	}
+	
+	//read letter from input
+	private char readLetter() {
+		char letter = ' ';
+		return letter;
+	}
+
+	//try a letter and replace in a guess
+	private void tryLetter(char a) {
+		
+	}
+	
+}
